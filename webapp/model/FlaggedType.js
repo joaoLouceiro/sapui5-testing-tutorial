1f8e854cd26a19aa5c8ec5e61d2c5aced9557dc0
Joao Louceiro <jrlouceiro@live.com>
2024-10-03 13:12:57 +0100

    Step 5: Adding a Flag Button

---
+++ webapp/model/FlaggedType.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/model/SimpleType"
+], function (SimpleType) {
+    "use strict";
+    return SimpleType.extend("sap.ui.demo.bulletinboard.model.FlaggedType", {
+        formatValue: function (value) {
+            return value == 1;
+        },
+        parseValue: function (value) {
+            return value == true ? 1 : 0;
+        },
+        validateValue: function () {
+        }
+    });
+});
